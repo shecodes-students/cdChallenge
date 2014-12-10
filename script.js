d503d2e966cb1bdf261ebb1c7ebde4b43b229f5c
Giorgia Sambrotta <g.sambrotta@gmail.com>
2014-12-10 23:09:18 +0100

    create a random tree with child and deep levels

--- script.js
+++ script.js
@@ -1,48 +1,27 @@
-
-var list = document.createElement('ul');
-
 var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
 
-document.body.appendChild(list);
-function randomTree() {
-    for (var i=0; i<directoryList.length; i++) {
-        var element = document.createElement('li');
-            directoryList.appendChild(element);
-            element.innerHTML = element.innerHTML + directoryList[i];
-    }		
-//first tree level fixed and equal to tree	
-var root = "/root";
-
-//conditions for depth, wideness and shallowness of the tree
-
-function Node (maxDepth, maxWide) {
-
-var maxDepth = 3; //tree can be 3 levels deep excluding root
-var maxBrunchWide = 3; //each node can have max 3 children
-
-// i didn't think for now on how to assure that just one element from the array is chosen 
-    this.name = randomFromDirectoryList(directoryList.length);
-    this.parentNode = function () {
-        do 
-        var paternityTest = randomFromDirectoryList(); 
-        while (paternityTest === this.name)
-        return paternityTest; // not sure what this do function returns!
-        // how do I make it so that the parentNode = output of function?
-    };
-    this.childrenNode = function() {
-	for (var i === 0; maxBrunchWide - 1; i++) {
-	this.childrenNode[i] = randomFromDirectoryList();
-	}	
-    };
-
-    function randomFromDirectoryList () {
-        var randomIndex = Math.floor(Math.random()*directoryList.length-1);
-        var random = directoryList[randomIndex];
-        return random;
-    }
-};
-
-
-//create random numbers between 1 and directoryList.length to pick array values
-
+function Node(maxDeep, maxBrothers){
+    //var random = directoryList[Math.floor(Math.random()*directoryList.length-1)];
+    if( maxDeep > 0 ){
+        for( var i = 0; i <= maxBrothers; i++ ){
+            this['child-' + i] = new Node(maxDeep - 1, 0); //0 become a random number
+        }
+    } return;
 }
+
+var randomNum = Math.floor( Math.random() * 10 );
+var randomBrothers = Math.floor( Math.random() * 5 );
+var node = new Node(randomNum, randomBrothers);
+console.log(node);
+
+/*
+var html = "";
+html.innerHTML =+ "<ul> /root";
+html.innerHTML =+ //node.render
+html.innerHTML =+ "</ul>";
+*/
+
+// node.render function
+//html.ChildNode = "<li>"+ random +"</li>";
+
+
