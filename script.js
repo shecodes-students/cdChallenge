6483ce855183575980e57040541c8b98ede668ec
Leonor Oliveira <leonorfmartins@gmail.com>
2014-12-10 18:05:06 +0100

    first attempt to create a a random name and a random parent for each node. For now, no considerations on how each name should be unique. not tested.

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 
 var list = document.createElement('ul');
+
 document.body.appendChild(list);
-var arrayList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
-for (var i=0; i<arrayList.length; i++) {
-    var element = document.createElement('li');
-    list.appendChild(element);
-    element.innerHTML = element.innerHTML + arrayList[i];
+function randomTree() {
+    var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
+    for (var i=0; i<directoryList.length; i++) {
+        var element = document.createElement('li');
+            directoryList.appendChild(element);
+            element.innerHTML = element.innerHTML + directoryList[i];
+    }		
+//first tree level fixed and equal to tree	
+var root = "/root";
+
+//conditions for depth, wideness and shallowness of the tree
+
+function Node (maxDepth, maxWide) {
+
+var maxDepth = 3; //tree can be 3 levels deep excluding root
+var maxBrunchWide = 3; //each node can have max 3 children
+
+// i didn't think for now on how to assure that just one element from the array is chosen 
+    this.name = randomFromDirectoryList(directoryList.length);
+    this.parentNode = function () {
+        do 
+        var paternityTest = randomFromDirectoryList(maxDepth); 
+        while (paternityTest === this.name)
+        return paternityTest; // not sure what this do function returns!
+        // how do I make it so that the parentNode = output of function?
+    };
+     this.childrenNode = randomFromDirectoryList(maxBrunchWide);
+
+    function randomFromDirectoryList (max) {
+        var randomIndex = Math.floor(Math.random()*max);
+        var random = directoryList[randomIndex];
+        return random;
+    }
+};
+
+
+//create random numbers between 1 and directoryList.length to pick array values
+
+
+
 
 }
-
