9b0a0562213131895c0fc57f943e026ec26415bf
Leonor Oliveira <leonorfmartins@gmail.com>
2015-02-19 16:34:05 +0100

    first attempt to create an uniqueKey property.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
 
+<<<<<<< Updated upstream
 function makeNode(name, maxGenerations, maxSiblings){
+=======
+var uniqueKey = 0;
+function makeNode(maxGenerations, maxSiblings, uniqueKey){
+>>>>>>> Stashed changes
     var node = {};
     node.name = name;
     if (maxGenerations > 0) {
@@ -11,6 +16,7 @@ function makeNode(name, maxGenerations, maxSiblings){
             var randomSiblings = Math.floor( Math.random() * maxSiblings );
             // we need a way to prevent the same random name to be generated twice
             // (because this would simply overwrite an existing propery)
+<<<<<<< Updated upstream
                 node[randomName] = makeNode(randomName, randomGenerations, randomSiblings);
 //                console.log(node[name]); 
         }
@@ -21,6 +27,27 @@ function makeNode(name, maxGenerations, maxSiblings){
 
 var node = makeNode("root",6,9);
 console.log(node);
+=======
+            uniqueKey ++;
+            node[randomName] = makeNode(randomGenerations, randomSiblings, uniqueKey);
+        }
+    }
+    return node;
+}
+
+function addRoot (node) {
+    var fileTree = {root: node};
+    console.log(fileTree);
+    console.log(fileTree.root);
+    return fileTree, uniqueKey;
+}
+addRoot(makeNode(3,5));
+
+function pickRandomStartAndTargetPosition (fileTree) {
+    var startObjectKey;  
+}
+
+>>>>>>> Stashed changes
 
 var html = "";
 html.text += "<ul> /root";
