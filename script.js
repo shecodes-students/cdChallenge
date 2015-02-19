c6889c2c84d3f8e402cb7f3e27dc1b00ab486e79
Leonor Oliveira <leonorfmartins@gmail.com>
2015-02-19 11:22:46 +0100

    small for loop correction

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about'
 function makeNode(maxGenerations, maxSiblings){
     var node = {};
     if (maxGenerations > 0) {
-        for (var i = 0; i <= maxSiblings; i++) {
+        for (var i = 0; i < maxSiblings; i++) {
             var randomGenerations = Math.floor( Math.random() * (maxGenerations-1));
+            console.log("RG:"+randomGenerations);
             var randomName = directoryList[Math.floor(Math.random()*directoryList.length)];
+            console.log("RN:"+randomName);
             var randomSiblings = Math.floor( Math.random() * maxSiblings );
+            console.log("RS:"+randomSiblings);
             // we need a way to prevent the same random name to be generated twice
             // (because this would simply overwrite an existing propery)
             node[randomName] = makeNode(randomGenerations, randomSiblings);
+            console.log("node"+node[randomName]);
         }
     }
     return;
 }
 
-var node = makeNode(5, 3);
-console.log(node);
+var test = makeNode(5, 3);
+console.log(test);
 
 /*
 var html = "";
