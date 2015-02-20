d12a7bb1173596b247137c8b3ec168e494538bed
Leonor Oliveira <leonorfmartins@gmail.com>
2015-02-20 20:41:17 +0100

    resolved merge conflict

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
 
-<<<<<<< Updated upstream
-function makeNode(name, maxGenerations, maxSiblings){
-=======
 var uniqueKey = 0;
 function makeNode(maxGenerations, maxSiblings, uniqueKey){
->>>>>>> Stashed changes
     var node = {};
     node.name = name;
     if (maxGenerations > 0) {
-        //while (node[randomName] != node[randomName])
         for (var i = 1; i <= maxSiblings; i++) {
             var randomGenerations = Math.floor( Math.random() * (maxGenerations-1));
             var randomName = directoryList[Math.floor(Math.random()*directoryList.length)];
             var randomSiblings = Math.floor( Math.random() * maxSiblings );
             // we need a way to prevent the same random name to be generated twice
             // (because this would simply overwrite an existing propery)
-<<<<<<< Updated upstream
-                node[randomName] = makeNode(randomName, randomGenerations, randomSiblings);
-//                console.log(node[name]); 
-        }
-    }
-    console.log("maxGenerations:" + maxGenerations+ ", maxSiblings:" + maxSiblings + " node:" + node);
-    return node;
-}
-
-var node = makeNode("root",6,9);
-console.log(node);
-=======
             uniqueKey ++;
             node[randomName] = makeNode(randomGenerations, randomSiblings, uniqueKey);
         }
@@ -47,7 +30,6 @@ function pickRandomStartAndTargetPosition (fileTree) {
     var startObjectKey;  
 }
 
->>>>>>> Stashed changes
 
 var html = "";
 html.text += "<ul> /root";
