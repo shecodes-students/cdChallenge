97464f1114b1e9550066e88e7d38ac9b2c240ff6
Leonor Oliveira <leonorfmartins@gmail.com>
2015-03-04 23:00:34 +0100

    added exports module. Created a working server for html and js files.

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var http = require('http');
-var nodeStatic = require('node-static');
-var file = nodeStatic.Server('./cdChallenge', {cache: 3600});
 var fs = require('fs');
-console.log(file);
-//var convert = require ('./script');
 http.createServer(function(req, res) {
-    req.addListener('end', function() {
-        file.serve(req, res, function(err, result) {
-            if (err) {
-                console.log('error'+ req.url+ err.message);
-            } else {
-                console.log('%s - %s'+ req.url+ res.message);
-            }
-        });
-    }).resume();
+    var contents;
+    if(req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        contents = fs.readFileSync('./index.html', 'UTF-8');
+        res.end(contents);
+        console.log('html');
+    }
+    if (req.url === '/script.js') {
+        res.writeHead(200, {'Content-Type': 'application/javascript'});
+        contents = fs.readFileSync('./script.js', 'UTF-8');
+        res.end(contents);
+        console.log('js');
+    } else {
+        res.writeHead(404);
+        res.end('oops');
+    }
 }).listen(3000);
