da665632d94dfa5ed123878099aec4e222eb0a05
Leonor Oliveira <leonorfmartins@gmail.com>
2015-03-04 15:49:38 +0100

    deleted jquery. Implemented meteor to setup a server.

---
+++ Meteor/cdChallenge/cdChallenge.js
@@ -0,0 +1,41 @@
+var nodeCollection = [];
+var directoryList = ['data', 'source', 'html', 'server','public','private', 'bin', 'user', 'etc', 'modules', 'temp'];
+var nestedList = '';
+function Node(maxGenerations, maxSiblings, name, parentNode) {
+    nodeCollection.push(this);
+    this.name = name;
+    this.parentNode = parentNode;
+    if (maxGenerations > 0) {
+        for (var i = 0; i < maxSiblings; i++) {
+            var randomGenerations = Math.floor( Math.random() * (maxGenerations-1));
+            var randomName; //defined here, otherwise JSlint triggers error
+            while (this[randomName]) { //make sure name isn't repeated
+                randomName = directoryList[Math.floor(Math.random()*directoryList.length)];
+            }
+            var randomSiblings = Math.floor( Math.random() * maxSiblings );
+            this[randomName] = new Node(randomGenerations, randomSiblings, randomName, this);
+        }
+    }
+
+    this.toHTML = function() { //probably have to do it recursively as well?
+        if (this.parentNode === '') {
+            nestedList = '<ul>' + this[name];
+        } else {
+            nestedList += '<li>' + this[randomName] + '</li>';
+        }
+        nestedList += '</ul>';
+    };
+    return nestedList;
+}
+var root = new Node(4,2,'root', '');
+console.log(nestedList);
+console.log(root.toHTML());
+
+module.exports = {
+    Node: Node,
+    nestedList : nestedList
+};
+
+//need to find a way to display nestedList result in the html
+
+
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require('http');
+var nodeStatic = require('node-static');
+var file = nodeStatic.Server('./cdChallenge', {cache: 3600});
 var fs = require('fs');
+console.log(file);
 //var convert = require ('./script');
 http.createServer(function(req, res) {
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('./index.html').pipe(res);
-    } else {
-        res.writeHead(404);
-        res.end('ups');
-    }
+    req.addListener('end', function() {
+        file.serve(req, res, function(err, result) {
+            if (err) {
+                console.log('error'+ req.url+ err.message);
+            } else {
+                console.log('%s - %s'+ req.url+ res.message);
+            }
+        });
+    }).resume();
 }).listen(3000);
