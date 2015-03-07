fdbdd303c65605f5fabfd8316765535bf65077a8
Leonor Oliveira <leonorfmartins@gmail.com>
2015-03-07 16:01:02 +0100

    Rethinking of some variable names. Restructure of code so that successfuly prints a complete random tree. First approach to display the tree on html.

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
-var directoryList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
+var nameList = ['data', 'text', 'home', 'index', 'style', 'custom', 'about', 'img', 'fonts', 'readme'];
+var inspect = require('util').inspect;
 
-function makeNode(maxGenerations, maxSiblings){
-    var node = {};
+function makeTree(name, maxGenerations, maxNumberOfChildren){
+    var node = {
+        name: name,
+        children: []
+    };
     if (maxGenerations > 0) {
-        for (var i = 0; i < maxSiblings; i++) {
-            var randomGenerations = Math.floor( Math.random() * (maxGenerations-1));
-            console.log("RG:"+randomGenerations);
-            var randomName = directoryList[Math.floor(Math.random()*directoryList.length)];
-            console.log("RN:"+randomName);
-            var randomSiblings = Math.floor( Math.random() * maxSiblings );
-            console.log("RS:"+randomSiblings);
+        var numberOfChildren = Math.floor( Math.random() * maxNumberOfChildren );
+        numberOfChildren = Math.min(numberOfChildren, nameList.length);
+
+        // create a copy of the big book of all names
+        var myNameList = nameList.slice(); 
+
+        for (var i = 0; i < numberOfChildren; i++) {
+            // we pick a random number that is guaranteed
+            // to be less than the maxGenerations argument.
+            var nextMaxGenerations = Math.floor( Math.random() * (maxGenerations-1));
+            var index = Math.floor(Math.random()*myNameList.length);
+            var childName = myNameList.splice(index,1)[0];
+            
             // we need a way to prevent the same random name to be generated twice
             // (because this would simply overwrite an existing propery)
-            node[randomName] = makeNode(randomGenerations, randomSiblings);
-            console.log("node"+node[randomName]);
+            var newChild = makeTree(childName, nextMaxGenerations, maxNumberOfChildren);
+            node.children.push(newChild);
         }
     }
-    return;
+    return node;
+}
+function createListOfPaths(tree, childrenPath) {
+   // childrenPath = [];
+    //childrenPath.push(tree.name);
+    for (var i=0;i<tree.children.length - 1; i++) {
+            
+        childrenPath.push(tree.name +'/' + childrenpath.join());//tree.children[i].name);
+        childrenPath = childrenPath.concat(createListOfPaths(tree, childrenPath));
+    }        
+    return childrenPath;
 }
 
-var test = makeNode(5, 3);
-console.log(test);
+function makeHTMLForTree(tree) {
+    var html = '<div>' + tree.name + '</div>';
+    html += '<ul>';
+    for (var i=0;i<tree.children.length; ++i) {
+        html += '<li>';
+        html += makeHTMLForTree(tree.children[i]);        
+        html += '</li>';
+    }        
+    html += '</ul>';
+    return html;
+}
 
-/*
-var html = "";
-html.innerHTML =+ "<ul> /root";
-html.innerHTML =+ //node.render
-html.innerHTML =+ "</ul>";
-*/
+var tree = makeTree('ROOT', 9, 13);
+console.log(inspect(tree, false, null, true));
 
-// node.render function
-//html.ChildNode = "<li>"+ random +"</li>";
-
+console.log(createListOfPaths(tree, []));
+//document.querySelector('#content').innerHTML = 'asdf';
